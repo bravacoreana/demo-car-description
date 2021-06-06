8baef4d3ae2ee1aa105a4544df8a38e096391df2
jungwoonlim <wypo23@naver.com>
2021-06-06 11:26:59 +0900

    initial setting

---
+++ README.md
@@ -0,0 +1,3 @@
+## Demo Car Description
+안녕하세요 구름 개발자 구름입니다.
+Three.js와 React를 이용한 간단한 차체 설명 페이지를 만들어보겠습니다.
---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <div>3D Model</div>
+      <div>Button</div>
+      <div>Sidebar</div>
+    </div>
+  );
+}
+
+export default App;
